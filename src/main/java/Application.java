d87d6caead4c1c17a0ffb9ba84e76656ce5d5f01
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-09-10 11:29:41 +0800

    添加程序入口

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableTransactionManagement
+public class Application{
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
